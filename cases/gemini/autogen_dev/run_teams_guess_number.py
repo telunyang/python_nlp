1bfb3995beb8e1aa3e39e86e32e813e4cdf9d72a
Darren Yang <telunyang@gmail.com>
2025-02-19 14:29:32 +0800

    2025.02.19 修改範例 (teacher and student guess a number between 1 to 100)

--- cases/gemini/autogen_dev/run_teams_guess_number.py
+++ cases/gemini/autogen_dev/run_teams_guess_number.py
@@ -8,28 +8,26 @@ from autogen_agentchat.teams import RoundRobinGroupChat
 from autogen_agentchat.ui import Console
 from autogen_core import CancellationToken
 from autogen_ext.models.openai import OpenAIChatCompletionClient
-from dotenv import load_dotenv
-load_dotenv(override=True)
 
-# 輸入你的 Google API Key
-api_key = os.getenv('GOOGLE_API_KEY')
-if not api_key:
-	raise ValueError('GOOGLE_API_KEY is not set')
 
-# 設定模型
-model = "gemini-1.5-flash-8b"
-
-# 模型變數初始化
+# 模型變數初始化 (在這裡使用 ollama，下載 llama 3.3 70b 量化模型)
 model_client = OpenAIChatCompletionClient(
-    model=model,
-    api_key=api_key
+    model="llama3.3:latest",
+    base_url="http://localhost:11434/v1",
+    api_key="placeholder",
+    model_info={
+        "vision": False,
+        "function_calling": True,
+        "json_output": False,
+        "family": "unknown",
+    },
 )
 
 # 建立 agent
 teacher = AssistantAgent(
     "teacher",
     model_client=model_client,
-    system_message="你是一位小學老師，正在跟學生在課堂上玩猜數字遊戲。"
+    system_message="你是一位小學老師，正在跟學生在課堂上玩猜數字遊戲。如果學生猜對，就說「猜對了」"
 )
 
 # 建立 agent
@@ -39,29 +37,13 @@ student_01 = AssistantAgent(
     system_message="你是一位小學生，正在跟老師在課堂上玩猜數字遊戲。"
 )
 
-# 建立 agent
-student_02 = AssistantAgent(
-    "student_02",
-    model_client=model_client,
-    system_message="你是一位小學生，正在跟老師在課堂上玩猜數字遊戲。"
-)
-
-# 建立 agent
-student_03 = AssistantAgent(
-    "student_03",
-    model_client=model_client,
-    system_message="你是一位小學生，正在跟老師在課堂上玩猜數字遊戲。"
-)
-
 # 定義一個終止條件，如果評論者允者，則停止任務。
-text_termination = TextMentionTermination("答對了")
+text_termination = TextMentionTermination("猜對")
 
 # 建立一個團隊
 team = RoundRobinGroupChat([
         teacher, 
         student_01,
-        student_02,
-        student_03
     ], 
 	termination_condition=text_termination
 )
@@ -69,14 +51,7 @@ team = RoundRobinGroupChat([
 # 執行團隊任務
 async def team_run() -> None:
     # 任務內容
-    task = '''請使用繁體中文來進行猜數字遊戲，數字在 1 到 100 之間，實際數字由你隨機決定。
-
-你會不斷地提示學生，學生猜的數字太大或太小，你會提醒他們實際數字在某個區間內，
-假設答案是 20：
-- 如果學生猜 35，你就會說「太大了，答案在 1 到 35 之間」，
-- 如果學生猜 6，你就會說「太小了，答案在 6 到 35 之間」。
-- 依此類推，直到學生猜對為止。
-'''
+    task = '''進行猜數字遊戲，數字在 1 到 100 之間，實際數字由你隨機決定。你會不斷地提示學生，學生猜的數字太大或太小，你會提醒他們實際數字在某個區間內，'''
 
     # 將先前的任務清除
     # await team.reset()
